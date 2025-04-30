6e5f9e89935c2b6e30590f9d0a1b0f8ca420b83b
Emma Blanchard <emma.blnch4@gmail.com>
2025-04-30 14:21:48 +0200

    mariadb container done

---
+++ srcs/requirements/mariadb/tools/initiate_databde.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Attendre que le daemon MariaDB soit prêt à accepter les connexions (optionnel mais plus propre)
+sleep 2
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+mysql -e "FLUSH PRIVILEGES;"
+
+exec mysqld_safe
