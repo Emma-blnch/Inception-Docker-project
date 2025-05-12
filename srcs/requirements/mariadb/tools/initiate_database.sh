193792c2202bb8c2fb722edd3a5d6fa97e110c72
Emma Blanchard <eblancha@f4r6s5.clusters.42paris.fr>
2025-05-12 15:37:50 +0200

    removed service on mariadb.sh

--- srcs/requirements/mariadb/tools/initiate_database.sh
+++ srcs/requirements/mariadb/tools/initiate_database.sh
@@ -1,24 +1,30 @@
 #!/bin/sh
 set -e
 
-service mysql start
+# service mysql start
 
-# Attendre que le daemon MariaDB soit prêt à accepter les connexions (optionnel mais plus propre)
-until mysqladmin ping --silent; do
-    sleep 1
-done
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+    echo "Initialisation de la base de donnees..."
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+    mysqld --initialize-insecure
+    mysqld_safe --skip-networking &
+    pid="$!"
 
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    # Attendre que le daemon MariaDB soit prêt à accepter les connexions (optionnel mais plus propre)
+    until mysqladmin ping --silent; do
+        sleep 1
+    done
 
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
+    mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+    mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mysql -e "FLUSH PRIVILEGES;"
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 
-mysql -e "FLUSH PRIVILEGES;"
-
-mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+    wait "$pid"
+fi
 
 echo "Lancement de la base de données..."
 exec mysqld_safe
