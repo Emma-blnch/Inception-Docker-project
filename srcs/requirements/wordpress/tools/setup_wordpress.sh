c197b9bb4e6ef5362ad37f31c83dd8a7d2c01a94
Emma Blanchard <eblancha@f4r6s5.clusters.42paris.fr>
2025-05-12 16:37:11 +0200

    modified volumes for persistance

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -4,23 +4,6 @@ set -e
 # le temps que mariadb soit prêt
 sleep 10
 
-# if [ ! -f /var/www/wordpress/wp-config.php ]; then
-#     echo "Création du fichier wp-config.php..."
-
-#     cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
-
-#     sed -i "s/database_name_here/${MYSQL_DATABASE}/" /var/www/wordpress/wp-config.php
-#     sed -i "s/username_here/${MYSQL_USER}/" /var/www/wordpress/wp-config.php
-#     sed -i "s/password_here/${MYSQL_PASSWORD}/" /var/www/wordpress/wp-config.php
-#     sed -i "s/localhost/${MYSQL_HOSTNAME}/" /var/www/wordpress/wp-config.php
-
-#     # Remplissage rapide des salts avec des chaînes aléatoires
-#     for salt in AUTH_KEY SECURE_AUTH_KEY LOGGED_IN_KEY NONCE_KEY AUTH_SALT SECURE_AUTH_SALT LOGGED_IN_SALT NONCE_SALT
-#     do
-#         RANDOM_KEY=$(openssl rand -base64 32)
-#         sed -i "s/define('$salt',.*);/define('$salt', '${RANDOM_KEY}');/" /var/www/wordpress/wp-config.php
-#     done
-# fi
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
     echo "Création du fichier wp-config.php..."
 
@@ -50,6 +33,8 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
         --role=author \
         --path=/var/www/wordpress \
         --allow-root
+
+    wp theme install twentytwentyfour --activate --path=/var/www/wordpress --allow-root
 
 fi
 
